21e5e441095a4d274ab1cfe881cab823fd9e9694
thais <tcorreia16@amherst.edu>
2014-02-09 16:13:20 -0500

    Trying to POST

--- public/js/main.js
+++ public/js/main.js
@@ -21,15 +21,50 @@ $(function() {
 		}
 	});
 
-	var event = new Event();
-	event.set("destination", "home");
-	event.save(null, {
-	  success: function(object) {
-	    console.log("Success");
-	  },
-	  error: function (model, error) {
-	    console.log("Error");
-	  }
-	});
+	// The Application
+	// ---------------
+
+	  var ManageAppView = Parse.View.extend({
+
+	    events: {
+	      "click #submit-btn":  "createEvent"
+	    },
+
+		initialize: function() 
+	      docu.event = new Event();
+	    },
+	    
+	    // If you hit return in the main input field, create new Todo model
+	    createEvent: function(e) {
+
+			var event = new Event();
+			event.set("destination", this.$("#dest").value());
+			event.save(null, {
+			  success: function(object) {
+			    console.log("Success");
+			  },
+			  error: function (model, error) {
+			    console.log("Error");
+			  }
+			});
+
+	      console.log("Event " + event.destination);
+	    }
+	  });
+
+	  // The main view for the app
+	  var AppView = Parse.View.extend({
+	    // Instead of generating a new element, bind to the existing skeleton of
+	    // the App already present in the HTML.
+	    el: $("#backsafeapp"),
+
+	    initialize: function() {
+	      this.render();
+	    },
+
+	    render: function() {
+	      new ManageAppView();
+	    }
+	  });
 
 });
