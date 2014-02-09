f4d72feed200438e85d56cba20085023ac539d22
thirab <hirab22t@mtholyoke.edu>
2014-02-09 15:30:12 -0500

    added fb user login
    cont

--- cloud/main.js
+++ cloud/main.js
@@ -68,7 +68,17 @@ Parse.Cloud.define("CheckSafe", function(request,response){
 });
 
 
-
-
+Parse.FacebookUtils.logIn(null, {
+  success: function(user) {
+    if (!user.existed()) {
+      alert("User signed up and logged in through Facebook!");
+    } else {
+      alert("User logged in through Facebook!");
+    }
+  },
+  error: function(user, error) {
+    alert("User cancelled the Facebook login or did not fully authorize.");
+  }
+});
 });
 
