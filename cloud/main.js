3dc890df87c971db01a5a59e438279a0911ca1af
thais <tcorreia16@amherst.edu>
2014-02-09 14:54:05 -0500

    Initial commit

---
+++ cloud/main.js
@@ -0,0 +1,34 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
+});
+
+Parse.Cloud.define("something", function(request,response){
+	response.sucess("something!");
+});
+
+Parse.Cloud.define("getFriends", function(request,response){
+	response.sucess("showFriends!");
+});
+
+Parse.Cloud.define("post", function(request,response){
+	response.sucess("Store Data!");
+});
+
+Parse.Cloud.define("CheckSafe", function(request,response){
+	response.sucess("check all events to see if any have expired past time expected to get back, and send mesages!");
+});
+
+
+var Event = Parse.Object.extend({
+	destination: "destination",
+	expectedBack: "expectedBack"
+	bufferTime: "buffer",
+	userDescription: "userDescription",
+	travelingWith: "travelingWith",
+	meetingWho: "meeting",
+	timeLeaving: "timeLeaving",
+	doing: "doing"
+});
