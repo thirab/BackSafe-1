6c0d9f9ebd74128f6fab895e2c165393c240e28a
thais <tcorreia16@amherst.edu>
2014-02-09 16:31:43 -0500

    Got POST

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,9 @@
-
-$(function() {
-
-  Parse.$ = jQuery;
-
-  // TASK 1: ENTER YOUR APPLICATION AND JAVASCRIPT KEYS HERE
+$(document).ready(function() {
+	
   Parse.initialize("E3aKqq2sCS2aC20YyNALua7YMGxMMZd71IJ0iTHb",
                    "NAU96IHJ6zOUUOpqaLVEj1dCMPv4H02N8B4SetPR");
 
-
-	var Event = Parse.Object.extend("Event",{
+  	var Event = Parse.Object.extend("Event",{
 		defaults: {
 			destination: "destination",
 			expectedBack: "expectedBack",
@@ -20,51 +15,26 @@ $(function() {
 			doing: "doing"
 		}
 	});
+	
+	$("#eventForm").on("submit", function(e) {
+		e.preventDefault();
 
-	// The Application
-	// ---------------
+		console.log("Handling the submit");
+		//add error handling here
+		//gather the form data
 
-	  var ManageAppView = Parse.View.extend({
+		var event = new Event();
+		event.dest = $("#dest").val();
 
-	    events: {
-	      "click #submit-btn":  "createEvent"
-	    },
-
-		initialize: function() 
-	      docu.event = new Event();
-	    },
-	    
-	    // If you hit return in the main input field, create new Todo model
-	    createEvent: function(e) {
-
-			var event = new Event();
-			event.set("destination", this.$("#dest").value());
-			event.save(null, {
-			  success: function(object) {
-			    console.log("Success");
-			  },
-			  error: function (model, error) {
-			    console.log("Error");
-			  }
-			});
-
-	      console.log("Event " + event.destination);
-	    }
-	  });
-
-	  // The main view for the app
-	  var AppView = Parse.View.extend({
-	    // Instead of generating a new element, bind to the existing skeleton of
-	    // the App already present in the HTML.
-	    el: $("#backsafeapp"),
-
-	    initialize: function() {
-	      this.render();
-	    },
-
-	    render: function() {
-	      new ManageAppView();
-	    }
-	  });
-
+		event.save(null, {
+			success:function() {
+				console.log("Success");
+			},
+			error:function(e) {
+				console.dir(e);
+			}
+		});
+		
+	});
+	
 });
