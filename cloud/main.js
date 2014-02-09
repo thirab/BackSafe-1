23f9bb71d40700ad4f2d47012c099c888cd02215
thirab <hirab22t@mtholyoke.edu>
2014-02-09 15:07:22 -0500

    setting event objects
    set up added extra values

--- cloud/main.js
+++ cloud/main.js
@@ -7,6 +7,22 @@ $(function() {
                    "cryRxrIRuA9LejD3xKIcg4XegnwzJLqErnm1ZH8X",
                    "jLs4QguNTMYHxHaBfOtw1U00g0xmn5MS6KDCnXJc");
 
+
+//define objects
+
+//events
+var Event = Parse.Object.extend({
+	destination: "destination",
+	expectedBack: "expectedBack"
+	bufferTime: "buffer",
+	userDescription: "userDescription",
+	travelingWith: "travelingWith",
+	meetingWho: "meeting",
+	timeLeaving: "timeLeaving",
+	doing: "doing"
+});
+
+
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
@@ -20,13 +36,15 @@ Parse.Cloud.define("something", function(request,response){
 Parse.Cloud.define("getFriends", function(request,response){
 	response.sucess("showFriends!");
 });
-
+Parse.Cloud.define("getEvents", function(user){
+//todo for user get event
+}
 Parse.Cloud.define("post", function(request,response){
-    new outEvent(
-    document.getElementById("back").value;
-    document.getElementById("wearing").value;
-    document.getElementById("with").value;
-    document.getElementById("meeting").value;
+    new outEvent = new Event();
+    outEvent.expectedBack=document.getElementById("back").value;
+    outEvent.userDescription = document.getElementById("wearing").value;
+    outEvent.travelingWith = document.getElementById("with").value;
+    outEvent.meeting = document.getElementById("meeting").value;
 
 });
 
@@ -35,16 +53,7 @@ Parse.Cloud.define("CheckSafe", function(request,response){
 });
 
 
-var Event = Parse.Object.extend({
-	destination: "destination",
-	expectedBack: "expectedBack"
-	bufferTime: "buffer",
-	userDescription: "userDescription",
-	travelingWith: "travelingWith",
-	meetingWho: "meeting",
-	timeLeaving: "timeLeaving",
-	doing: "doing"
-});
+
 
 });
 
